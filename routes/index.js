68857dda704691e79c4ce4d509bc3f5b0930e911
Jiang <jiang@MAC-HOJIANG.local>
2017-08-03 11:33:28 +0800

    Add 'api' for url.

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,16 @@ const projectRoutes = require('./projects');
 const memberRoutes = require('./members');
 const debtorRoutes = require('./debtors');
 
-const VERSION = 'v1';
+const VERSION = '/api/v1';
 
 const loadRoutes = function(app) {
-    app.use(`/${VERSION}/records`, recordRoutes);
-    app.use(`/${VERSION}/income`, incomeRoutes);
-    app.use(`/${VERSION}/outcome`, outcomeRoutes);
-    app.use(`/${VERSION}/accounts`, accountRoutes);
-    app.use(`/${VERSION}/projects`, projectRoutes);
-    app.use(`/${VERSION}/members`, memberRoutes);
-    app.use(`/${VERSION}/debtors`, debtorRoutes);
+    app.use(`${VERSION}/records`, recordRoutes);
+    app.use(`${VERSION}/income`, incomeRoutes);
+    app.use(`${VERSION}/outcome`, outcomeRoutes);
+    app.use(`${VERSION}/accounts`, accountRoutes);
+    app.use(`${VERSION}/projects`, projectRoutes);
+    app.use(`${VERSION}/members`, memberRoutes);
+    app.use(`${VERSION}/debtors`, debtorRoutes);
 };
 
 module.exports = loadRoutes;
